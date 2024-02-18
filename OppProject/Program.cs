7179fef4427b992e63544a450b1cee38c5ca854f
durdubaltaci7 <139330782+durdubaltaci7@users.noreply.github.com>
2024-02-18 13:59:37 +0300

    Add project files.

---
+++ OppProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml;
+
+namespace OppProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mpt = new MPT
+            {
+                SilahModel = "MPT-76",
+                MühimmatKapasite = 10,
+                Agirlik = 1.5,
+                SatisFiyat = 500
+            };
+
+            var piyadeTüfeği = new MilliPiyade
+            {
+                SilahModel = "MPT-55",
+                MühimmatKapasite = 20,
+                Agirlik = 2.0,
+                SatisFiyat = 600
+            };
+
+            var AK = new Keleş
+            {
+                SilahModel = "AK-47",
+                MühimmatKapasite = 30,
+                Agirlik = 3.0,
+                SatisFiyat = 7000
+            };
+
+            var silahlar = new List<Silah> { mpt, piyadeTüfeği, AK };
+
+            foreach (var silah in silahlar)
+            {
+                Console.WriteLine($"{silah.SilahModel,-25}{silah.MühimmatKapasite,-20}{silah.Agirlik,-15}{silah.SatisFiyat}");
+            }
+        }
+        
+    }
+}
---
+++ OppProject/Silah.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppProject
+{
+    public abstract class Silah
+    {
+        public required string SilahModel { get; set; }
+        public int MühimmatKapasite { get; set; }
+        public double Agirlik { get; set; }
+        public decimal SatisFiyat { get; set; }
+
+        public abstract void AtesEt();
+        public abstract void Doldur();
+        public abstract void Temizle();
+    }
+
+    public class MPT : Silah
+    {
+        public override void AtesEt()
+        {
+            Console.WriteLine($"{SilahModel} MPT ateş edildi.");
+        }
+
+        public override void Doldur()
+        {
+            Console.WriteLine($"{SilahModel} MPT dolduruldu.");
+        }
+
+        public override void Temizle()
+        {
+            Console.WriteLine($"{SilahModel} MPT temizlendi.");
+        }
+    }
+
+    public class MilliPiyade : Silah
+    {
+        public override void AtesEt()
+        {
+            Console.WriteLine($"{SilahModel} MilliPiyade tüfeği ates edildi.");
+        }
+
+        public override void Doldur()
+        {
+            Console.WriteLine($"{SilahModel} MilliPiyade tüfeği dolduruldu.");
+        }
+
+        public override void Temizle()
+        {
+            Console.WriteLine($"{SilahModel} MilliPiyade tüfeği temizlendi.");
+        }
+    }
+
+    public class Keleş : Silah
+    {
+        public override void AtesEt()
+        {
+            Console.WriteLine($"{SilahModel} Keleş ateş edildi.");
+        }
+
+        public override void Doldur()
+        {
+            Console.WriteLine($"{SilahModel} Keleş dolduruldu.");
+        }
+
+        public override void Temizle()
+        {
+            Console.WriteLine($"{SilahModel} Keleş temizlendi.");
+        }
+    }
+}
